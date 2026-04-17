016963a2cc120961a9708dd9a94e5b7cb187f18c
charl024 <comaoeng2003@gmail.com>
2026-04-17 16:28:09 -0600

    removed nn.Sequential, reverted stuff 2

--- rnn_gru.py
+++ rnn_gru.py
@@ -5,16 +5,14 @@ class RNNGRU(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(RNNGRU, self).__init__()
 
-        self.rnn_process = nn.Sequential(
-
-            nn.GRU(input_size, hidden_size),
-
-            nn.Linear(hidden_size, output_size),
-            nn.LogSoftmax(dim=1)
-        )
+        self.rnn = nn.GRU(input_size, hidden_size)
+        self.h2o = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
 
 
     def forward(self, line_tensor):
-        output = self.rnn_process(line_tensor)
+        rnn_out, hidden = self.rnn(line_tensor)
+        output = self.h2o(hidden[0])
+        output = self.softmax(output)
         return output
 
